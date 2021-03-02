7a956f0cae2f011f8d52584493aa6a802bb124ea
sherandrei <welldryus@gmail.com>
2021-03-03 02:09:13 +0300

    task F done

---
+++ DataAnalysisIntro/Lection1/HW/task6.py
@@ -0,0 +1,19 @@
+def counter_to_words():
+    counter = dict()
+    for word in list(map(str.lower, input().split())):
+        if word not in counter:
+            counter[word] = 0
+        counter[word] += 1
+    return counter
+
+
+def compare_counters(first, second):
+    for word in second:
+        if word not in first:
+            return False
+        if first[word] < second[word]:
+            return False
+    return True
+
+
+print("YES" if compare_counters(counter_to_words(), counter_to_words()) else "NO")
