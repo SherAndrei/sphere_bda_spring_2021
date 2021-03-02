1c7b9fc96bcc70e348c24d2bb7229844d10a85b7
sherandrei <welldryus@gmail.com>
2021-03-02 23:06:36 +0300

    task A done

--- DataAnalysisIntro/Lection1/HW/task1.py
+++ DataAnalysisIntro/Lection1/HW/task1.py
@@ -1,10 +1,7 @@
 start_size = int(input())
-str_numbers = input().split()
 my_set = set()
-for str_number in str_numbers:
-    new_number = int(str_number)
-    if new_number not in my_set:
-        print(new_number, end=" ")
-    my_set.add(new_number)
-print()
-print(start_size - len(my_set))
+for number in [int(i) for i in input().split()]:
+    if number not in my_set:
+        print(number, end=" ")
+    my_set.add(number)
+print('', start_size - len(my_set), sep='\n')
