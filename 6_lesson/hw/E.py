a7f4a118bd2854dfb2a08edafe872d98b8a3a987
sherandrei <welldryus@gmail.com>
2021-04-16 22:09:15 +0300

    E hw3

--- 6_lesson/hw/E.py
+++ 6_lesson/hw/E.py
@@ -1,37 +1,9 @@
-# %%
-from itertools import tee
-from itertools import zip_longest
-
 def chain_loop(args):
-    for tuple in zip_longest(*args):
-        for elem in tuple:
-            yield elem
-
-
-if __name__ == '__main__':
-    a = range(5)
-    b = range(10)
-    c = range(3)
-    print(list(chain_loop([a, b, c])))
-    print([0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9])
-
-    print("===================")
-
-    a = [None, None, None]
-    b = [1] * 5
-    print(list(chain_loop([a, b])))
-    print([None, 1, None, 1, None, 1, 1, 1])
-
-    print("===================")
-
-    a = (i for i in range(10))
-    b = a
-    print(list(chain_loop([a, b])))
-    print([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-    print("===================")
-
-    a = (i for i in range(3))
-    print(list(chain_loop(tee(a, 5))))
-    print([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2])
-# %%
+    is_finished = {iter(arg): False for arg in args}
+    while False in is_finished.values():
+        unfinished = filter(lambda elem: elem[1] is False, is_finished.items())
+        for it, _ in unfinished:
+            try:
+                yield next(it)
+            except StopIteration:
+                is_finished[it] = True
